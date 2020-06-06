9dc057caf896c26ae1b59d15fb1f7db9af966c7b
Austin Keener <keeneraustin@yahoo.com>
2020-06-06 11:51:52 -0400

    Initial commit of base repository

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'one-second-everyday'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
